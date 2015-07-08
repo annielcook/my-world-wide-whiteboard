c37b2a71853b2870b7e09c9d189a0cdc2c0508be
GregAMeyer <gregmeyer888@gmail.com>
2015-07-08 15:58:28 -0400

    able to draw on others

---
+++ browser/app.js
@@ -0,0 +1,17 @@
+
+// This object describes the URL of the page we're on!
+var socket = io(window.location.origin);
+
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!');
+});
+
+whiteboard.on('draw', function(start, end, strokeColor){
+	console.log(start, end, strokeColor)
+	socket.emit("mouseLocation", start, end, strokeColor)
+})
+
+socket.on('otherArtistMouse', function(start, end, strokeColor){
+	whiteboard.draw(start, end, strokeColor)
+})
+
--- server.js
+++ server.js
@@ -6,14 +6,38 @@ var server = http.createServer();
 var express = require('express');
 var app = express();
 
-server.on('request', app);
+var socketio = require('socket.io');
 
-server.listen(1337, function () {
+server.on('request', app);
+var io = socketio(server);
+// creates a new connection server for web sockets and integrates it into our HTTP server
+
+io.on('connection', function(socket) {
+    // This function receives the newly connected socket.
+    // This function will be called for EACH browser that connects to our server.
+    console.log('A new client has connected!');
+    //console.log(socket.id);
+    //console.log("These are all the connections: ", io.sockets)
+
+    socket.on('disconnect', function(socket) {
+        console.log('Boo! Someone left!');
+    });
+
+    socket.on('mouseLocation', function(start, end, strokeColor) {
+        //console.log("This is mouse: ", mouseLocation);
+        socket.broadcast.emit("otherArtistMouse", start, end, strokeColor )
+    });
+
+});
+
+
+
+server.listen(1337, function() {
     console.log('The server is listening on port 1337!');
 });
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
